97115cb30bfde743c364e91738200fb30c683a26
Denys Haryachyy <garyachy@gmail.com>
2014-06-19 17:50:59 +0300

    edge.aggregator: Added two more hosts to mininet topology
    Signed-off-by: Denys Haryachyy <garyachy@gmail.com>

--- pox/edge/agg_net.py
+++ pox/edge/agg_net.py
@@ -22,12 +22,16 @@ def aggNet():
 
     h1 = net.addHost( 'h1', ip='10.0.0.1' )
     h2 = net.addHost( 'h2', ip='10.0.0.2' )
+    h3 = net.addHost( 'h3', ip='10.0.0.3' )
+    h4 = net.addHost( 'h4', ip='10.0.0.4' )
     s1 = net.addSwitch( 's1' )
     s2 = net.addSwitch( 's2' )
 
 
     net.addLink( h1, s1 )
-    net.addLink( h2, s2 )
+    net.addLink( h2, s1 )
+    net.addLink( h3, s2 )
+    net.addLink( h4, s2 )
 
     # Create GRE tunnels
     s1.cmd('ovs-vsctl add-port s1 tun1 -- set Interface tun1 type=gre options:remote_ip=NODE2_IP options:local_ip=NODE1_IP')
